663e17962fb3dbc51c5871212c260410669b7a75
divisha-jn <divishajain999@gmail.com>
2025-04-13 16:21:27 +0800

    Update Login.java and LoginCLI.java

--- Login/Login.java
+++ Login/Login.java
@@ -1,1 +1,79 @@
+package Login;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Login{
+    public static ArrayList<String[]> chooseRole(int role){
+         ArrayList<String[]> userData = null;
+
+        switch (role) {
+            case 1:
+                userData = readCSV("ApplicantList.csv");
+                break;
+            case 2:
+                userData = readCSV("OfficerList.csv");
+                break;
+            case 3:
+                userData = readCSV("ManagerList.csv");
+                break;
+
+        }
+        return userData;
+    }
+
+
+    private static ArrayList<String[]> readCSV(String filePath) {
+        ArrayList<String[]> data = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                data.add(line.split(","));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    public static String[] checkNRIC(String NRIC, ArrayList<String[]> userData) { 
+        if (userData == null) {
+            System.out.println("Error: No data loaded.");
+            return null; 
+        }
+    
+        for (int i = 1; i < userData.size(); i++) {
+            String[] row = userData.get(i);
+            if (row.length > 1 && NRIC.equals(row[1])) {
+                System.out.println("Valid NRIC");
+                return row; 
+            }
+        }
+    
+        System.out.println("Invalid NRIC. Please enter valid NRIC:");
+        return null; 
+    }
+
+
+    public static boolean checkPassword(String password, String[] userRow) { 
+        if (userRow == null) {
+            System.out.println("Error: No data loaded."); 
+            return false;
+        }
+    
+        if (userRow.length > 4 && password.equals(userRow[4])) { 
+            System.out.println("You have logged in!");
+            return true;
+        }
+    
+        System.out.println("Invalid password. Please enter valid password:");
+        return false;
+    }
+}
+
 
--- cli/LoginCLI.java
+++ cli/LoginCLI.java
@@ -1,9 +1,47 @@
 package cli;
 
+import java.util.ArrayList;
 import java.util.Scanner;
-public class LoginCLI {
-    
-    static Scanner scan = new Scanner(System.in);
+import Login.Login;
 
+public class LoginCLI {
+    public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);  
+		System.out.println("1. Applicant");
+		System.out.println("2. Officer");
+		System.out.println("3. Manager");
+        System.out.print("Choose a role:");
+        
+        int role = scan.nextInt();
+        scan.nextLine();
+        ArrayList<String[]> data = new ArrayList<>();
+        data=Login.chooseRole(role);
+
+        String[] matchedUser = null; 
+        boolean validNRIC = false;
+        
+        System.out.print("Please enter NRIC: ");
+        do {
+            String NRIC = scan.nextLine();
+            matchedUser = Login.checkNRIC(NRIC, data); 
+            validNRIC = (matchedUser != null); 
+        } while (!validNRIC);
+        
+        System.out.print("Please enter password: ");
+        boolean validPassword;
+        do {
+            String password = scan.nextLine();
+            validPassword = Login.checkPassword(password, matchedUser); 
+        } while (!validPassword);
+
+        System.out.println("1. Access Role");
+        System.out.println("2. Change Passsword");
+        System.out.print("Please enter which action you would like to perform: ");
+        
+
+
+
+    }
     
 }
