bd67771e2fcdc0ca6b69a563902cf98c5e1a0929
lynette <lynettexqchow@gmail.com>
2025-04-16 14:34:45 +0800

    Update Enquiry.java

--- Actors/Enquiry.java
+++ Actors/Enquiry.java
@@ -4,6 +4,7 @@ import Actors.User; // Base class
 import Actors.Applicant;
 import Actors.Officer;
 import Actors.Manager;
+import Actors.Reply;
 import Project.Project;
 
 import java.util.ArrayList;
