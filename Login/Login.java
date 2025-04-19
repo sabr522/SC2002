bf7e6eea743ac3259b6de399a07db5f426f6194c
sabr522 <100817023+sabr522@users.noreply.github.com>
2025-04-20 02:33:54 +0800

    Update Login.java
    Add Javadoc Comments

--- Login/Login.java
+++ Login/Login.java
@@ -10,6 +10,10 @@ public class Login {
     /**
      * Validates user login based on NRIC and password.
      * Returns the User object if successful, or null if credentials are invalid.
+     * @param nric User NRIC
+     * @param password Password input
+     * @param usersMap Map of all users
+     * @return Matching User object or null
      */
     public static User authenticate(String nric, String password, Map<String, User> usersMap) {
         
@@ -26,6 +30,10 @@ public class Login {
 
     /**
      * Updates the password of the given user.
+     * @param user The user object
+     * @param newPassword New password input
+     * @param confirmPassword Confirmation input
+     * @return true if password updated successfully
      */
     public static boolean updatePassword(User user, String newPassword, String confirmPassword) {
         if (!newPassword.equals(confirmPassword)) {
